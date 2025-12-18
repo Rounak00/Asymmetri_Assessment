7a03083f45ecf3bcf0163aa29350fcd7c04ebe11
Rounak00 <mukherjeerounak10@gmail.com>
2025-12-18 18:18:28 +0530

    Dev: Implemented the Github Authentication and Google Authentication successfully

---
+++ src/app/login/page.tsx
@@ -0,0 +1,58 @@
+import { auth, signIn } from "@/config/authHandler";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { redirect } from "next/navigation";
+
+export default async function LoginPage() {
+  // check the sesssion here
+  const session = await auth();
+  if (session?.user) {
+    redirect("/");
+  }
+
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-gray-50">
+      <Card className="w-[350px] shadow-lg">
+        <CardHeader className="text-center">
+          <CardTitle className="text-2xl font-bold">Asymmetri AI</CardTitle>
+          <CardDescription>
+            Sign in to start chatting
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-4">
+          
+          {/* gitHub login Button */}
+          <form
+            action={async () => {
+              "use server";
+              await signIn("github", { redirectTo: "/" });
+            }}
+          >
+            <Button className="w-full" variant="outline" type="submit">
+              Sign in with GitHub
+            </Button>
+          </form>
+
+          {/* Google login button */}
+          <form
+            action={async () => {
+              "use server";
+              await signIn("google", { redirectTo: "/" });
+            }}
+          >
+            <Button className="w-full" variant="outline" type="submit">
+              Sign in with Google
+            </Button>
+          </form>
+
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
+import { auth, signOut } from "@/config/authHandler";
 import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return ( 
-  <>
-   <h1>AI Assistant Ready</h1>
-    <Button variant="default">Shadcn Button Works!</Button>
-  </>
-  )
+export default async function Home() {
+  // session check from db
+  const session = await auth();
+  if (!session?.user) {
+    redirect("/login");
+  }
+
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <h1 className="text-2xl font-bold">
+        Welcome back, {session.user.name}!
+      </h1>
+      <p className="text-gray-500">You are securely logged in.</p>
+      
+      {/* Signout button */}
+      <form
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: "/login" });
+        }}
+      >
+        <Button variant="destructive">Sign Out</Button>
+      </form>
+    </div>
+  );
 }
